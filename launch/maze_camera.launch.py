42f82d91fbf4372345750c894a6c75415edda919
Ghostbird-07 <anshulchoudhary931@gmail.com>
2025-04-11 00:14:08 +0530

    idk what are these i will change it

--- launch/maze_camera.launch.py
+++ launch/maze_camera.launch.py
@@ -3,37 +3,36 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import ExecuteProcess
 from launch_ros.actions import Node
-from scripts import GazeboRosPaths
 
 def generate_launch_description():
     package_share_dir = get_package_share_directory("mazebot")
     urdf_file = os.path.join(package_share_dir, "urdf", "mazebot.urdf")
     world_file = os.path.join(package_share_dir, "world", "mazeworld.world")
-
-    model_path, plugin_path, media_path = GazeboRosPaths.get_paths()
+    
+    # Properly set Gazebo paths
     env = {
-        "GAZEBO_MODEL_PATH": model_path, # as we only to add maze_bot(model) into gazebo models path
-        "GAZEBO_PLUGIN_PATH": plugin_path,
-        "GAZEBO_RESOURCE_PATH": media_path,
+        "GAZEBO_MODEL_PATH": os.path.join(get_package_share_directory("mazebot"), "models") + ":" + os.environ.get("GAZEBO_MODEL_PATH", ""),
+        "GAZEBO_PLUGIN_PATH": os.environ.get("GAZEBO_PLUGIN_PATH", ""),
+        "GAZEBO_RESOURCE_PATH": os.path.join(get_package_share_directory("mazebot")) + ":" + os.environ.get("GAZEBO_RESOURCE_PATH", ""),
+        # Force software rendering
+        "LIBGL_ALWAYS_SOFTWARE": "1",
+        # For debugging
+        "GAZEBO_DEBUG": "1"
     }
-    return LaunchDescription(
-        [
-            ExecuteProcess(
-                cmd=["gazebo","-- verbose",world_file,"-s","libgazebo_ros_factory.so",],
-                output="screen",
-                additional_env=env,
-            ),
-            # Node(
-            #     package="gazebo_ros",
-            #     executable="spawn_entity.py",
-            #     arguments=["-entity","mazebot","-b","-file", urdf_file,
-            #     ],
-            # ),
-            Node(
-                package="robot_state_publisher",
-                executable="robot_state_publisher",
-                output="screen",
-                arguments=[urdf_file],
-            ),
-        ]
-    )
+    
+    return LaunchDescription([
+        # Use gzserver instead of gazebo (headless)
+        ExecuteProcess(
+            cmd=["gzserver", "--verbose", world_file, "-s", "libgazebo_ros_factory.so"],
+            output="screen",
+            additional_env=env,
+        ),
+        
+        # Robot state publisher
+        Node(
+            package="robot_state_publisher",
+            executable="robot_state_publisher",
+            output="screen",
+            arguments=[urdf_file],
+        ),
+    ])
--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup
 from glob import glob
 import os
+
 package_name = 'mazebot'
 
 setup(
@@ -11,26 +12,24 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share',package_name,'urdf'),glob('urdf/*')),
-        (os.path.join('share',package_name,'meshes'),glob('meshes/*')),
-        (os.path.join('share',package_name,'launch'),glob('launch/*')),
-        (os.path.join('share',package_name,'world'),glob('world/*')),
+        (os.path.join('share', package_name,'launch'), glob('launch/*')),
+        (os.path.join('share', package_name,'urdf'), glob('urdf/*')),
+        (os.path.join('share', package_name,'meshes'), glob('meshes/*')),
+        (os.path.join('share', package_name,'world'), glob('worlds/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='niran',
-    maintainer_email='nirans2002@gmail.com',
-    description='maze bot',
-    license='Apache License 2.0',
+    maintainer='luqman',
+    maintainer_email='noshluk2@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-             'talker = mazebot.publisher_member_function:main',
-             'listener = mazebot.subscriber_member_function:main',
-             'drive_maze_bot = mazebot.driving_node:main',
-             'go_to_goal = mazebot.go_to_goal:main',
-             'video_saver_node= mazebot.video_saver:main',
-             'maze_solver= mazebot.maze_solver:main',
+            'driving_node = mazebot.driving:main' ,
+            'go_to_goal = mazebot.go_to_goal:main',
+            'video_recorder = mazebot.video_saver:main',
+            'maze_solver = mazebot.maze_solver:main',
         ],
     },
 )
